ba10874801ce0deb51d1a361d0acc62db8ab0993
Richard Lewis <richard.lewis@gold.ac.uk>
2014-01-30 16:03:36 +0000

    Added MusicCircle::MediaType class stub

---
+++ lib/MusicCircle/MediaType.pm
@@ -0,0 +1,21 @@
+#
+# Transforming Musicology -- Music Circle
+#
+# Music Circle is a social music collaboration tool developed by
+# Matthew Yee-King and colleagues on the PRAISE project at Goldsmiths'
+# College, University of London. This project implements a similar
+# Web-based musical commenting system, but working with existing
+# musical works rather than uploaded audio performances.
+#
+# Authors: Richard Lewis <richard.lewis@gold.ac.uk>
+#
+# Copyright (C) 2013 Richard Lewis, Goldsmiths' College
+
+package MusicCircle::MediaType;
+
+use Moose;
+use namespace::autoclean;
+
+__PACKAGE__->meta->make_immutable;
+
+1;

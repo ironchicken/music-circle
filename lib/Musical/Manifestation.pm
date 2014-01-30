d1cbc5173c535382d9ebce29db9b8c65c45dc5ed
Richard Lewis <richard.lewis@gold.ac.uk>
2014-01-30 16:11:05 +0000

    Added properties to Musical::Manifestation class

--- lib/Musical/Manifestation.pm
+++ lib/Musical/Manifestation.pm
@@ -16,7 +16,118 @@ package Musical::Manifestation;
 use Moose;
 use namespace::autoclean;
 
-__PACKAGE__->meta->make_immutable;
+with qw(MooseX::Semantic::Role::WithRdfType);
+with qw(MooseX::Semantic::Meta::Attribute::Trait);
 
+use RDF::Trine::Namespace qw(rdf xsd);
+use Musical qw($mo);
+use FRBR qw($frbr);
+use FOAF;
+use MusicCircle::MediaType;
+
+__PACKAGE__->rdf_type($mo->MusicalManifestation);
+
+has 'embodiment_of' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Musical::Expression',
+    uri => $frbr->embodiment_of,
+    );
+
+has 'alternate' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Musical::Manifestation',
+    uri => $frbr->alternate,
+    );
+
+has 'alternate_of' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Musical::Manifestation',
+    uri => $frbr->alternate_of,
+    );
+
+has 'compilation_of' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'ArrayRef[Musical::Signal]',
+    uri => $mo->compilation_of,
+    );
+
+has 'compiler' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'FOAF::Agent',
+    uri => $mo->compiler,
+    );
+
+has 'media_type' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'MusicCircle::MediaType',
+    uri => $mo->media_type,
+    );
+
+has 'other_release_of' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Musical::Manifestation',
+    uri => $mo->other_release_of,
+    );
+
+has 'preview' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Musical::Item',
+    uri => $mo->preview,
+    );
+
+has 'producer' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'FOAF::Agent',
+    uri => $mo->producer,
+    );
+
+has 'publication_of' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Musical::Expression',
+    uri => $mo->publication_of,
+    );
+
+has 'publisher' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'FOAF::Agent',
+    uri => $mo->publisher,
+    );
+
+has 'publishing_location' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Str',
+    uri => $mo->publishing_location,
+    rdf_datatype => $xsd->string,
+    );
+
+has 'release_status' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Str',
+    uri => $mo->release_status,
+    rdf_datatype => $xsd->string,
+    );
+
+has 'release_type' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Str',
+    uri => $mo->release_type,
+    rdf_datatype => $xsd->string,
+    );
+
+__PACKAGE__->meta->make_immutable;
 
 1;

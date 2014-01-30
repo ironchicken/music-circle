22459f7501a88c4b938d116b565d2eb8a69fd7c8
Richard Lewis <richard.lewis@gold.ac.uk>
2014-01-30 16:01:54 +0000

    Added properties to Musical::Expression class

--- lib/Musical/Expression.pm
+++ lib/Musical/Expression.pm
@@ -13,8 +13,67 @@
 
 package Musical::Expression;
 
+# This package defines the Musical::Expression class which corresponds
+# with the Music Ontology class mo:MusicalExpression.
+
 use Moose;
 use namespace::autoclean;
+
+with qw(MooseX::Semantic::Role::WithRdfType');
+with qw(MooseX::Semantic::Meta::Attribute::Trait);
+
+use RDF::Trine::Namespace qw(rdf xsd);
+use Musical qw($mo);
+use FRBR;
+
+__PACKAGE__->rdf_type($mo->MusicalExpression);
+
+has 'realization_of' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Musical::Work',
+    uri => $frbr->realization_of,
+    );
+
+has 'abridgement_of' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Musical::Expression',
+    uri => $frbr->abridgement_of,
+    );
+
+has 'revision_of' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Musical::Expression',
+    uri => $frbr->revision_of,
+    );
+
+has 'translation_of' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Musical::Expression',
+    uri => $frbr->translation_of,
+    );
+
+has 'arrangement_of' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'Musical::Expression',
+    uri => $frbr->arrangement_of,
+    );
+
+has 'realizer' => (
+    traits => ['Semantic'],
+    is => 'rw',
+    isa => 'FRBR::ResponsibleEntity',
+    uri => $frbr->realizer,
+    );
+
+has 'dimensions' => (
+    is => 'rw',
+    isa => 'ArrayRef[Musical::Dimension]',
+    );
 
 __PACKAGE__->meta->make_immutable;
 
